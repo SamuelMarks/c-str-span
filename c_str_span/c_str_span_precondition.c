cf6d5b353ce8f0d278a3ce23ab2c01d5e562d41b
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-04-29 22:32:03 -0400

    [c_str_span/c_str_span_precondition.c] Revert `AZ_ON_FAIL_DEBUG`; [CMakeLists.txt] Flatten `foreach` loop; [c_str_span/CMakeLists.txt] Exclude `${PROJECT_NAME}_compiler_flags` from install target

--- c_str_span/c_str_span_precondition.c
+++ c_str_span/c_str_span_precondition.c
@@ -2,21 +2,13 @@
  * SPDX-License-Identifier: MIT */
 
 #include "c_str_precondition_internal.h"
-#if AZ_ON_FAIL_DEBUG
-#include <stdio.h>
-#endif /* AZ_ON_FAIL_DEBUG */
 
 /*#include <azure/core/_az_cfg.h>*/
 
 static void az_precondition_failed_default(void)
 {
   /* By default, when a precondition fails the calling thread spins forever */
-  while (1)
-  {
-#if AZ_ON_FAIL_DEBUG
-    fputs("az_precondition_failed_default", stderr);
-#endif /* AZ_ON_FAIL_DEBUG */
-  }
+  while (1) {}
 }
 
 az_precondition_failed_fn _az_precondition_failed_callback = az_precondition_failed_default;
